a541ef93948fe1c9ea4aedd2d9e9e7b5921c8209
codewithhuzaifa <huzaifaarab2002@gmail.com>
2023-06-24 19:35:23 +0500

    nested-if & list

--- week2/class2  - list & nestedIF.dart
+++ week2/class2  - list & nestedIF.dart
@@ -1,5 +1,8 @@
+// <> this is call 'angle brackets'
+// => this is called 'arrow function'
+
 void main() {
-  //nested if statement
+  // nested if statement
   int number = 20;
   if (number % 5 == 0 || number % 7 == 0) {
     if (number % 5 == 0 && number % 7 == 0) {
@@ -12,4 +15,45 @@ void main() {
   } else {
     print("Number is not divisible by 5 or 7");
   }
+
+  List names = ['Huzaifa', 'Asim', 'Qadir', 'Fardeen'];
+  print(names);
+
+  print(names.length);
+
+  names.sort();
+  print(names);
+
+  print(names.reversed);
+  print(names);
+
+  names.add('Ali');
+  print(names);
+
+  names.addAll(['Asif', 'Faraz']);
+  print(names);
+
+  names.insert(
+    1,
+    'Hamza',
+  );
+  print(names);
+
+  names.remove('Hamza');
+  print(names);
+
+  names.removeAt(5);
+  print(names);
+
+  print(names.contains('Asim'));
+
+  print(names.where((e) => e == 'Fardeen')); //print name when e = fardeen
+
+  names.replaceRange(1, 2, ['Qureshi']);
+  print(names);
+
+  List<String> stname = [
+    'Huzaifa',
+  ]; // only string data can be assigned in this list.
+  print(stname);
 }
